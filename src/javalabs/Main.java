07a5ac120578466c2ee1e7cc85afea339ae1cb2e
Brian Frost <brianfrost@consolidated.net>
2018-07-09 22:28:26 -0500

    Added new features.

--- src/javalabs/Main.java
+++ src/javalabs/Main.java
@@ -4,17 +4,21 @@
 package javalabs;
 
 /**
- * @author brian
+ * @author Brian Frost
  *
  */
 public class Main {
 
 	/**
-	 * @param args
+	 * @param args - What's your name?
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, World");
+		System.out.print("Hello, ");
+		for (int i = 0; i < args.length; ++i) {
+			System.out.print(args[i] + " ");
+		}
+		System.out.println("");
 	}
 
 }
